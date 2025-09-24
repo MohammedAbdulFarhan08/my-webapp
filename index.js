833444a0c39ea6533e7394488e260ad4f4b9b0d9
MohammedAbdulFarhan08 <mohdfarhantech08@gmail.com>
2025-09-25 02:12:54 +0530

    -

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from public folder
+// Serve static files from public folder-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
